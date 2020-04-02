991a21427e4030a08d999602b9eb45cee73f205e
Ольга Шиманская <ol_shiman@mail.ru>
2020-04-02 19:56:55 +0600

    Added fixes

--- src/LLang.hs
+++ src/LLang.hs
@@ -1,17 +1,18 @@
 module LLang where
 
 import AST (AST (..), Operator (..))
-import Combinators (satisfy, Parser (..))
+import Combinators (Result (..), satisfy, Parser (..))
 import Expr (parseExpr, toOperator, parseIdent)
 import Control.Applicative
 import Control.Monad (guard)
+import Data.Maybe 
 
 type Expr = AST
 
 type Var = String
 data LAst
   = If { cond :: Expr, thn :: LAst, els :: LAst }
-  | While { cond :: AST, body :: LAst }
+  | While { cond :: Expr, body :: LAst }
   | Assign { var :: Var, expr :: Expr }
   | Read { var :: Var }
   | Write { expr :: Expr }
@@ -21,17 +22,20 @@ data LAst
 stmt :: LAst
 stmt =
   Seq
-    [ Read "X"
+    [ Read ("X")
     , If (BinOp Gt (Ident "X") (Num 13))
          (Write (Ident "X"))
          (While (BinOp Lt (Ident "X") (Num 42))
-                (Seq [ Assign "X"
+                (Seq [ Assign ("X")
                         (BinOp Mult (Ident "X") (Num 7))
                      , Write (Ident "X")
                      ]
                 )
          )
     ]
+
+subset :: [Var] -> [Var] -> Bool
+subset xs ys = all (\x -> x `elem` ys) xs
 
 removeSpaces :: String -> String
 removeSpaces = concat . words
@@ -49,6 +53,12 @@ parseVar = do
     var <- parseIdent
     guard (not (elem var keywords))
     return var
+
+getVarsE :: Expr -> [Var]
+getVarsE (BinOp _ a b) = getVarsE a ++ getVarsE b
+getVarsE (UnaryOp _ e) = getVarsE e
+getVarsE (Num _) = []
+getVarsE (Ident x) = [x]
 
 parseIf :: Parser String String LAst
 parseIf = do 
@@ -72,17 +82,18 @@ parseWhile = do
 
 parseAssign :: Parser String String LAst
 parseAssign = do
-    parseString "Assign"
+    parseString "Assign("
     var <- parseVar
-    parseString "("
+    parseString ")("
     expr <- parseExpr
     parseString ")"
     return (Assign var expr)
 
 parseRead :: Parser String String LAst
 parseRead = do
-    parseString "Read"
+    parseString "Read("
     var <- parseVar
+    parseString ")"
     return (Read var)
 
 parseWrite :: Parser String String LAst
@@ -102,5 +113,20 @@ parseSeq = do
 parseStat :: Parser String String LAst
 parseStat = parseIf <|> parseWhile <|> parseRead <|> parseWrite <|> parseAssign <|> parseSeq
 
+checkCorrectness :: LAst -> Bool
+checkCorrectness ast = isJust $ check ast [] 
+  where check :: LAst -> [Var] -> Maybe [Var]
+        check (If cond e1 e2) vars = if (subset (getVarsE cond) vars && isJust (check e1 vars) && isJust (check e2 vars)) then (Just vars) else Nothing
+        check (While cond body) vars = if (subset (getVarsE cond) vars && isJust (check body vars)) then (Just vars) else Nothing
+        check (Write expr) vars = if (subset (getVarsE expr) vars) then (Just vars) else Nothing
+        check (Seq stmts) vars = foldl f (Just vars) stmts
+               where f Nothing st = Nothing
+                     f (Just vars) st = check st vars
+        check (Read var) vars = Just $ if (var `elem` vars) then vars else (var:vars)
+        check (Assign var expr) vars = if (subset (getVarsE expr) vars) then (Just (if (elem var vars) then vars else (var:vars))) else Nothing
+
 parseProg :: Parser String String LAst
-parseProg = Parser $ \input -> runParser parseSeq (removeSpaces input)
+parseProg = Parser $ \input -> check $ runParser parseSeq (removeSpaces input)
+            where check s@(Success _ ast) | True <- checkCorrectness ast = s
+                                          | otherwise = Failure "Incorrect programm"
+                  check f = f
--- test/Test/LLang.hs
+++ test/Test/LLang.hs
@@ -27,7 +27,7 @@ unit_parseIf = do
 unit_parseWhile :: Assertion
 unit_parseWhile = do
     let p = Parser $ (runParser parseWhile) . removeSpaces
-    runParser p "While (x > 0) (Seq {Write (x); Assign x (x - 1);})" @?= Success "" (While {
+    runParser p "While (x > 0) (Seq {Write (x); Assign (x) (x - 1);})" @?= Success "" (While {
        cond = BinOp Gt (Ident "x") (Num 0),
        body = Seq [Write (Ident "x"), Assign "x" (BinOp Minus (Ident "x") (Num 1))]})
     runParser p "While (x) (Seq {})" @?= Success "" (While {
@@ -40,22 +40,24 @@ unit_parseWhile = do
 unit_parseAssign :: Assertion
 unit_parseAssign = do
      let p = Parser $ (runParser parseAssign) . removeSpaces
-     runParser p "Assign x (y)" @?= Success "" (Assign "x" (Ident "y"))
-     runParser p "Assign _123 (42/6) " @?= Success "" (Assign "_123" (BinOp Div (Num 42) (Num 6)))
+     runParser p "Assign (x) (y)" @?= Success "" (Assign "x" (Ident "y"))
+     runParser p "Assign (_123) (42/6) " @?= Success "" (Assign "_123" (BinOp Div (Num 42) (Num 6)))
      assertBool "" $ isFailure $ runParser p "Assign () (Seq { })"
      assertBool "" $ isFailure $ runParser p "Assign _@34 (Seq { })"
      assertBool "" $ isFailure $ runParser p "Assign x"
      assertBool "" $ isFailure $ runParser p "Assign Assign"
      assertBool "" $ isFailure $ runParser p "Assign Seq"
+     assertBool "" $ isFailure $ runParser p "Assignx"
 
 unit_parseRead :: Assertion
 unit_parseRead = do
      let p = Parser $ (runParser parseRead) . removeSpaces
-     runParser p "Read x" @?= Success "" (Read "x")
-     runParser p "Read _123" @?= Success "" (Read "_123")
+     runParser p "Read (x)" @?= Success "" (Read "x")
+     runParser p "Read (_123)" @?= Success "" (Read "_123")
      assertBool "" $ isFailure $ runParser p "Read ("
      assertBool "" $ isFailure $ runParser p "Read 1write"
      assertBool "" $ isFailure $ runParser p "Read While"
+     assertBool "" $ isFailure $ runParser p "Readn"
 
 unit_parseWrite :: Assertion
 unit_parseWrite = do
@@ -71,7 +73,7 @@ unit_parseSeq = do
      let p = Parser $ (runParser parseSeq) . removeSpaces
      runParser p "Seq {Write (x);}" @?= Success "" (Seq [Write (Ident "x")])
      runParser p "Seq {}" @?= Success "" (Seq [])
-     runParser p "Seq {Write (x); Read x;}" @?= Success "" (Seq [Write (Ident "x"), Read "x"])
+     runParser p "Seq {Write (x); Read (x);}" @?= Success "" (Seq [Write (Ident "x"), Read "x"])
      assertBool "" $ isFailure $ runParser p "Seq {Write (x) }"
      assertBool "" $ isFailure $ runParser p "Seq {Seq }}"
 
@@ -79,13 +81,22 @@ unit_parseProg :: Assertion
 unit_parseProg = do
      let p = Parser $ (runParser parseProg) . removeSpaces
      runParser p "Seq {}" @?= Success "" (Seq [])
-     runParser p "Seq {Read n; Assign i (1); While (n > 0) (Seq {Assign i (n*i); Assign n (n-1);}); Write (i);}" @?= Success "" (Seq [
+     runParser p "Seq {Read (n); Assign (i) (1); While (n > 0) (Seq {Assign (i) (n*i); Assign (n) (n-1);}); Write (i);}" @?= Success "" (Seq [
         Read "n",
         Assign "i" (Num 1),
         While (BinOp Gt (Ident "n") (Num 0)) (Seq [Assign "i" (BinOp Mult (Ident "n") (Ident "i")), Assign "n" (BinOp Minus (Ident "n") (Num 1))]),
         Write (Ident "i")])
+     runParser p "Seq {Read (n); Assign (i) (1); Assign (i) (n*i); Assign (n) (n - 1);}" @?= Success "" (Seq [
+        Read "n", 
+        Assign "i" (Num 1), 
+        Assign "i" (BinOp Mult (Ident "n") (Ident "i")),
+        Assign "n" (BinOp Minus (Ident "n") (Num 1))])
+     runParser p "Seq {Read (n); Assign (i) (1); While (n > 0) (Seq {Write (n);}); Write (i);}" @?= Success "" (Seq [
+        Read "n", Assign "i" (Num 1), While (BinOp Gt (Ident "n") (Num 0)) (Seq [Write (Ident "n")]), Write (Ident "i")])
      assertBool "" $ isFailure $ runParser p "{}"
      assertBool "" $ isFailure $ runParser p "Read 4"
      assertBool "" $ isFailure $ runParser p "Seq {Write (3); If (x > 0);}"
      assertBool "" $ isFailure $ runParser p "Seq {Assign Seq; Write Seq; }"
-
+     assertBool "" $ isFailure $ runParser p "Seq {Write (x);}"
+     assertBool "" $ isFailure $ runParser p "Seq {Assign (x) (x + 1);}"
+     assertBool "" $ isFailure $ runParser p "Seq {Assign (i) (1); Assign (n) (n*i);}"
