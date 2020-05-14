5f5b9bcd652cf3d585387684d61f3ee8fa75cca3
Ольга Шиманская <ol_shiman@mail.ru>
2020-05-14 10:12:17 +0600

    task 1 is done

--- src/Regexp.hs
+++ src/Regexp.hs
@@ -8,13 +8,25 @@ data Regexp = Empty
             | Seq Regexp Regexp
             | Alt Regexp Regexp
             | Star Regexp
-            deriving (Eq, Ord)
+            deriving (Show, Eq, Ord)
 
 match :: Regexp -> String -> Bool
 match r s = nullable (foldl (flip derivative) r s)
 
 derivative :: Char -> Regexp -> Regexp
-derivative = error "derivative not implemented"
+derivative a reg = case reg of
+                    Empty -> Empty
+                    Epsilon -> Empty
+                    Char b -> if (a == b) then Epsilon else Empty
+                    Seq reg1 reg2 -> if (nullable reg1) then (Alt (Seq (derivative a reg1) reg2) (derivative a reg2)) else (Seq (derivative a reg1) reg2)  
+                    Alt reg1 reg2 -> Alt (derivative a reg1) (derivative a reg2)
+                    Star r -> Seq (derivative a r) reg
 
 nullable :: Regexp -> Bool
-nullable = error "nullable not implemented"
+nullable reg = case reg of
+          Empty -> False
+          Epsilon -> True
+          Char b -> False
+          Seq reg1 reg2 -> (nullable reg1) && (nullable reg2)
+          Alt reg1 reg2 -> (nullable reg1) || (nullable reg2)
+          Star r -> True
